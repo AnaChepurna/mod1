ee5260d519504d85beca9fe106bba1405315cb3b
ZabrotskyDmitry <reezee512@gmail.com>
2019-02-09 21:30:56 +0200

    some info

---
+++ main.cpp
@@ -0,0 +1,6 @@
+// https://www.codeproject.com/Articles/14154/OpenGL-Terrain-Generation-An-Introduction
+// https://medium.com/@edkins.sarah/set-up-sdl2-on-your-mac-without-xcode-6b0c33b723f7
+// https://github.com/itoral/gl-terrain-demo
+//
+// g++ -c main.cpp -I $HOME/mod1/SFML2.5.1/include
+// g++ main.o -o sfml-app -L $HOME/mod1/SFML2.5.1/lib -lsfml-graphics -lsfml-window -lsfml-system
